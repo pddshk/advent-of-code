5aff67d59b7562817795e8a40988f4fffde2fac8
pddshk <pav2001pav@gmail.com>
2025-12-07 21:47:15 +0100

    optimize day 7

--- 2025/Day07/main.jl
+++ 2025/Day07/main.jl
@@ -1,49 +1,41 @@
-using DataStructures
 using Test
 
 const filename = joinpath(@__DIR__, "input.txt")
 
 function parseinput(filename)
-    permutedims(stack(Vector{Char}, eachline(filename)))
+    mat = stack(Vector{Char}, eachline(filename))  # do not transpose as columns work better in Julia
+    mat = mat[:, any.(!=('.'), eachcol(mat))]
+    mat = map(mat) do c
+        if c == '.'
+            0
+        elseif c == '^'
+            -1
+        else
+            1
+        end
+    end
+    return mat
 end
 
-parseinput_result = parseinput(filename)
-
 function trace_beam!(mat)
-    q = Queue{CartesianIndex{2}}()
-    s = findfirst(==('S'), mat)
-    enqueue!(q, s)
-    mat[s] = '.'
-    # visited = Set{CartesianIndex{2}}()
-    dir = CartesianIndex(1, 0)
+    m, n = size(mat)
+    indexleft(ind) = ind + CartesianIndex(0, -1)
     cnt = 0
-    while !isempty(q)
-        pos = dequeue!(q)
-        # if pos in visited
-        #     continue
-        # end
-        # push!(visited, pos)
-        current = mat[pos]
-        if current == '.'
-            mat[pos] = '|'
-            newpos = pos + dir
-            if checkbounds(Bool, mat, newpos)
-                enqueue!(q, newpos)
-            end
-        elseif current == '^'
-            cnt += 1
-            for d in (CartesianIndex(0, -1), CartesianIndex(0, 1))
-                newpos = pos + d
-                if checkbounds(Bool, mat, newpos)
-                    enqueue!(q, newpos)
-                end
+    for ind in CartesianIndices((1:m, 2:n))  # skip first col
+        if mat[ind] != -1
+            mat[ind] += max(0, mat[indexleft(ind)])
+        else
+            cnt += mat[indexleft(ind)] != 0
+            for d in (CartesianIndex(-1, 0), CartesianIndex(1, 0))
+                newind = ind + d
+                mat[newind] += mat[indexleft(ind)]
             end
         end
     end
-    return cnt
+    return (part1_result=cnt, part2_result=sum(x -> max(x, 0), mat[:, end]))         
 end
 
-@testset "trace_beam!" begin
+@testset "day 7" begin
     mat = parseinput(IOBuffer("""
     .......S.......
     ...............
@@ -61,79 +53,7 @@ end
     ...............
     .^.^.^.^.^...^.
     ..............."""))
-    @test trace_beam!(mat) == 21
+    @test trace_beam!(mat) == (part1_result=21, part2_result=40)
 end
 
-part1_result = let mat = parseinput(filename)
-    trace_beam!(mat)
-end
-
-function transformmap(mat)
-    map(mat) do c
-        if c == '.'
-            0
-        elseif c == '^'
-            -1
-        else
-            -2
-        end
-    end
-end
-
-function trace_beam2!(mat)
-    q = Queue{CartesianIndex{2}}()
-    visited = Set{CartesianIndex{2}}()
-    s = findfirst(==(-2), mat)
-    enqueue!(q, s)
-    mat[s] = 1
-    dir = CartesianIndex(1, 0)
-    while !isempty(q)
-        pos = dequeue!(q)
-        if pos in visited
-            continue
-        end
-        push!(visited, pos)
-        nextpos = pos + dir
-        if !checkbounds(Bool, mat, nextpos)
-            continue
-        end
-        if mat[nextpos] != -1
-            mat[nextpos] += mat[pos]
-            enqueue!(q, nextpos)
-        else
-            for d in (CartesianIndex(0, -1), CartesianIndex(0, 1))
-                newpos = nextpos + d
-                if checkbounds(Bool, mat, newpos)
-                    mat[newpos] += mat[pos]
-                    enqueue!(q, newpos)
-                end
-            end
-        end
-    end
-    return sum(mat[end, :])
-end
-
-@testset "trace_beam2!" begin
-    mat = parseinput(IOBuffer("""
-    .......S.......
-    ...............
-    .......^.......
-    ...............
-    ......^.^......
-    ...............
-    .....^.^.^.....
-    ...............
-    ....^.^...^....
-    ...............
-    ...^.^...^.^...
-    ...............
-    ..^...^.....^..
-    ...............
-    .^.^.^.^.^...^.
-    ...............""")) |> transformmap
-    @test trace_beam2!(mat) == 40
-end
-
-part2_result = let mat = parseinput(filename) |> transformmap
-    trace_beam2!(mat)
-end
+trace_beam!(parseinput(filename))
