b867a9f86d20cec744d7abb2cffd5cd6b28be91e
pddshk <pav2001pav@gmail.com>
2025-12-11 22:10:09 +0100

    day 11: solve part 2

--- 2025/Day11/main.jl
+++ 2025/Day11/main.jl
@@ -52,9 +52,48 @@ part1(g, enumerated) = count_paths(g, "you", "out", enumerated)
     ggg: out
     hhh: ccc fff iii
     iii: out"""))
-    part1(g, enumerated) == 5
+    @test part1(g, enumerated) == 5
 end
 
 g, enumerated = parseinput(filename)
 
 part1_result = part1(g, enumerated)
+
+function part2(g, enumerated)
+    topo = topological_sort(g)
+    dac = findfirst(==(enumerated["dac"]), topo)
+    fft = findfirst(==(enumerated["fft"]), topo)
+    if dac < fft
+        svr2dac = count_paths(g, "svr", "dac", enumerated, topo)
+        dac2fft = count_paths(g, "dac", "fft", enumerated, topo)
+        fft2out = count_paths(g, "fft", "out", enumerated, topo)
+        return svr2dac * dac2fft * fft2out
+    else
+        svr2fft = count_paths(g, "svr", "fft", enumerated, topo)
+        fft2dac = count_paths(g, "fft", "dac", enumerated, topo)
+        dac2out = count_paths(g, "dac", "out", enumerated, topo)
+        return svr2fft * fft2dac * dac2out
+    end
+end
+
+@testset "part 2" begin
+    g, enumerated = parseinput(IOBuffer("""
+    svr: aaa bbb
+    aaa: fft
+    fft: ccc
+    bbb: tty
+    tty: ccc
+    ccc: ddd eee
+    ddd: hub
+    hub: fff
+    eee: dac
+    dac: fff
+    fff: ggg hhh
+    ggg: out
+    hhh: out"""))
+    @test part2(g, enumerated) == 2
+end
+
+part2_result = part2(g, enumerated)
+
+println("Part 1: ", part1_result, "\nPart 2: ", part2_result)
