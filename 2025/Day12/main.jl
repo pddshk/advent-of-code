f26c5c74f16343452b5e01d9da3f2eda60ce3f25
pddshk <pav2001pav@gmail.com>
2025-12-12 17:51:25 +0100

    solve day 12

--- 2025/Day12/main.jl
+++ 2025/Day12/main.jl
@@ -1,5 +1,34 @@
+using LinearAlgebra
 using Test
 
 const filename = joinpath(@__DIR__, "input.txt")
 
-parseinput(filename) = read(filename, String)
+struct Region
+    width::Int
+    length::Int
+    shapes::NTuple{6, Int}
+end
+
+function parseinput(filename)
+    shape = r"\d:\n(?<shape>(?:[.#]{3}\n){3})"
+    raw = read(filename, String)
+    shapes = map(eachmatch(shape, raw)) do m
+        stack(line -> [c == '#' for c in line], eachline(IOBuffer(m["shape"])))
+    end
+    region = r"(?<w>\d+)x(?<l>\d+): (?<nums>(?:\d+ ?)+)"
+    regions = map(eachmatch(region, raw)) do m
+        w = parse(Int, m["w"])
+        l = parse(Int, m["l"])
+        nums = parse.(Int, split(m["nums"])) |> Tuple
+        Region(w, l, nums)
+    end
+    return shapes, regions
+end
+
+shapes, regions = parseinput(filename)
+
+count(regions) do region
+    area = region.width * region.length
+    to_fit_area = 7sum(region.shapes)
+    area >= to_fit_area
+end |> println
